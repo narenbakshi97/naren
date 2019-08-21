7ee3990da6504e03a5aab4d439bab3dbb1a2b38b
narenbaksh97 <narenbakshi97@gmail.com>
2019-08-21 12:22:04 -0400

    First commit

---
+++ js/init.js
@@ -0,0 +1,44 @@
+function analyze(){
+		artyom.fatality();
+		setTimeout(function(){// if you use artyom.fatality , wait 250 ms to initialize again.
+	         artyom.initialize({
+	            lang:"hi-IN",// A lot of languages are supported. Read the docs !
+	            continuous:false,// recognize 1 command and stop listening !
+	            listen:true, // Start recognizing
+	            debug:true, // Show everything in the console
+	            speed:1 // talk normally
+	        	}).then(function(){
+	            	console.log("Ready to work !");
+	        	});
+	    	},250);
+		document.getElementById("mike").style.color = "#fff";
+	}
+
+	var speak = false;
+
+	// var d = setInterval(function(){
+	// 	if(artyom.isSpeaking()){
+	//     	speak = true;
+	// 	}
+	// 	else{
+	// 		speak = false;
+	// 	}
+	// },200);
+
+
+		function proceed()
+		{
+			document.getElementById("mike").style.color = "red";
+
+			setTimeout(function(){// if you use artyom.fatality , wait 250 ms to initialize again.
+	         artyom.initialize({
+	            lang:"en-US",// A lot of languages are supported. Read the docs !
+	            continuous:false,// recognize 1 command and stop listening !
+	            listen:true, // Start recognizing
+	            debug:true, // Show everything in the console
+	            speed:1 // talk normally
+	        	}).then(function(){
+	            	console.log("Ready to work !");
+	        	});
+	    	},250);
+	}
